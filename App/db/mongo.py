37998e74c24c7374ea92a55a989a4ef00fcdba87
Jimmy Sumba <jimmy.sumba.est@tecazuay.edu.ec>
2025-12-24 11:22:03 -0500

    .

--- App/db/mongo.py
+++ App/db/mongo.py
@@ -1,8 +1,11 @@
 from pymongo import MongoClient
 import streamlit as st
-
+from db.modelo import RegistroAtencion
 
 def get_mongo_client():
     uri = "mongodb+srv://Adrian_bd:Administrador31.@base.f1r4j33.mongodb.net/Base?retryWrites=true&w=majority"
     client = MongoClient(uri)
     return client
+
+def insertar_registro_atencion(registro_atencion:RegistroAtencion):
+    pass
